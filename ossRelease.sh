1a3a30c52759ebc188cffb8655972e6fe4b98737
MORILLE Jerome <morille.jerome@groupagrica.com>
2021-04-30 11:46:18 +0200

    Update oss release script

--- ossRelease.sh
+++ ossRelease.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -e
 
 VERSION=${1:-0.0.1-SNAPSHOT}
+VERSION=${1}
+ if [[ -z "${VERSION}" ]]; then
+    read -p "Version à releaser ? " VERSION
+  fi
 
 git co -b prepare-release
 mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$VERSION
@@ -10,3 +14,9 @@ git tag -a ossrh-$VERSION -m "Release version $VERSION"
 git push origin ossrh-$VERSION
 git checkout master
 git branch -D prepare-release
+
+# Not work pour 0-M3
+git merge ossrh-$VERSION
+mvn org.codehaus.mojo:versions-maven-plugin:2.7:set -DgenerateBackupPoms=false -DnextSnapshot=true
+git commit -am "Prepare Next Snapshot"
+git push origin master
